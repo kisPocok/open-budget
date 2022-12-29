3e9bd14f760fc605b7a0875c05ddc488c6131471
David Schneidhoffer <david.schneidhoffer@gmail.com>
2022-12-29 02:16:30 +0100

    Initialize repo

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    testEnvironment: 'node',
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    roots: [
+      "<rootDir>",
+      "/Users/david.schneidhoffer/www/budget",
+      "/Users/david.schneidhoffer/www/budget/app"
+    ],
+    modulePaths: [
+      "<rootDir>",
+      "/Users/david.schneidhoffer/www/budget",
+      "/Users/david.schneidhoffer/www/budget/node_modules"
+    ],
+    moduleDirectories: [
+      "node_modules",
+      "app"
+    ]
+};
